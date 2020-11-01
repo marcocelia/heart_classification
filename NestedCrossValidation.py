9382a4e407a8e6d92b10bd3e42fa4d8215f21ba9
Marco Celia <mcelia@minervanetworks.com>
2020-11-01 11:05:04 +0100

    - Generalized Nested cross validation to linear and non-linear kernels - Add decorator to measure elapsed time for nested_cross_validation

--- NestedCrossValidation.py
+++ NestedCrossValidation.py
@@ -2,92 +2,83 @@ import numpy as np
 from numpy import mean
 from numpy import std
 from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.model_selection import  GridSearchCV, KFold
 from sklearn.metrics import accuracy_score
 import pandas as pd
+from elapsed_time import timeit
 
-df = pd.read_excel ('heart.xlsx', sheet_name ='heart_train')
+@timeit
+def nested_cross_validation(*,x_feat, y_target, grid_hyper_param, n_splits=10, kernel, **kwargs):
 
-X_features = np.array(df.drop([df.columns[0], df.columns[-1]],axis=1).to_numpy())
+    # Configure the outer cross-validation class
+    outerCV = KFold(n_splits=n_splits)
 
-y_target = np.array(df[["Label"]].to_numpy())
+    # Array to store outer cross validation scores
+    outerScores=np.array([])
 
+    for train_outer,test_outer in outerCV.split(x_feat):
 
-# Grid values for C
+        curr_x_outer_trainset = x_feat[train_outer]
+        curr_y_outer_trainset = y_target[train_outer]
+        curr_x_outer_testset = x_feat[test_outer]
+        curr_y_outer_testset = y_target[test_outer]
 
-gridHyperPar = {"C": [0.001, 0.01, 0.1, 1, 10, 100]}
+        # Configure the inner cross validation class
 
-# Configure the outer cross-validation class
+        innerCV = KFold(n_splits=n_splits, shuffle=True, random_state=1)
 
-outerCV = KFold(n_splits=10)
+        if (kernel == "linear"):
+            svmModel = LinearSVC(
+                random_state=0,
+                tol=1e-5,
+                dual=False,
+                max_iter = 5000
+            )
+        else:
+            svmModel = SVC(kernel=kernel)
 
-# Array to store outer cross validation scores
+        # The inner cross validation is performed by the GridSearch function
+        gridSearch = GridSearchCV(
+            svmModel,
+            grid_hyper_param,
+            scoring='accuracy',
+            cv=innerCV,
+            refit=True
+        )
 
-outerScores=np.array([])
+        # Execute search
+        searchResult = gridSearch.fit(
+            curr_x_outer_trainset,
+            curr_y_outer_trainset.ravel()
+        )
 
+        # Given that refit=True, gridSearch.fit returns the best
+        # hyperParameter over the all curr_x_outer_trainset
+        best_model = searchResult.best_estimator_
+        best_c = gridSearch.best_params_['C']
 
-for trainOuter,testOuter in outerCV.split(X_features):
+        # We can now evaluate current model on the hold out dataset curr_x_outer_testset
+        current_y_Predictions = best_model.predict(curr_x_outer_testset)
 
-    current_X_Outer_TrainingSet = X_features[trainOuter]
+        # Compute accuracy
+        currentScore = accuracy_score(
+            curr_y_outer_testset,
+            current_y_Predictions
+        )
 
-    current_X_Outer_TestSet = X_features[testOuter]
+        # Append to array of scores
+        outerScores = np.append(outerScores, currentScore)
 
-    current_y_Outer_TrainingSet = y_target[trainOuter]
+        # Report progress
+        if (kernel == "rbf"):
+            best_gamma = gridSearch.best_params_['gamma']
+            print('Accuracy = %.3f, Best C = %.3f, Best Gamma = %.3f' % (currentScore, best_c, best_gamma))
+        else:
+            print('Accuracy = %.3f, Best C = %.3f' % (currentScore, best_c))
 
-    current_y_Outer_TestSet = y_target[testOuter]
-
-    # Configure the inner cross validation class
-
-    innerCV = KFold(n_splits=3, shuffle=True, random_state=1)
-
-    # svmModel = SVC(kernel="linear")
-
-    svmModel = LinearSVC(random_state=0,
-                     tol=1e-5,dual=False,
-                     max_iter = 5000)
-
-
-    # The inner cross validation is performed by the
-    # by the GridSearch function
-
-
-    gridSearch = GridSearchCV(svmModel,
-                              gridHyperPar,
-                              scoring='accuracy',
-                              cv=innerCV, refit=True)
-
-    # Execute search
-
-    searchResult = gridSearch.fit(current_X_Outer_TrainingSet,
-                                  current_y_Outer_TrainingSet.ravel())
-
-
-    # Given that refit=True, gridSearch.fit returns the best
-    # hyperParameter over the all current_X_Outer_TrainingSet
-
-    best_model = searchResult.best_estimator_
-
-    bestHyperParameter = gridSearch.best_params_['C']
-
-    # We can now evaluate current model
-    # on the hold out dataset current_X_Outer_TestSet
-
-    current_y_Predictions = best_model.predict(current_X_Outer_TestSet)
-
-    # Compute accuracy
-
-    currentScore = accuracy_score(current_y_Outer_TestSet,
-                                 current_y_Predictions)
-
-    # Append to array of scores
-
-    outerScores = np.append(outerScores, currentScore)
-
-	# Report progress
-
-    print('Accuracy = %.3f, Best Hyper Parameter = %.3f' % (currentScore, bestHyperParameter))
-
-print('Accuracy = %.3f (%.3f)' % (mean(outerScores), std(outerScores)))
+    print('Accuracy = %.3f (%.3f)' % (mean(outerScores), std(outerScores)))
+    return (mean(outerScores), std(outerScores))
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,34 @@
+import numpy as np
 from split_dataset import split_dataset
+from NestedCrossValidation import nested_cross_validation
 
 train_sample, test_sample = split_dataset(dataset_name='heart', label_name='Label', train_per=0.8)
 
+X_features = np.array(train_sample.drop(columns=['Label'],axis=1).to_numpy())
+y_target = np.array(train_sample[["Label"]].to_numpy())
+
+# Grid values for C
+gridHyperPar = {"C": [0.001, 0.01, 0.1, 1, 10, 100]}
+elapsed_time = {}
+accuracy, std_dev = nested_cross_validation(
+    x_feat = X_features,
+    y_target = y_target,
+    grid_hyper_param = gridHyperPar,
+    n_splits=10,
+    kernel='linear',
+    func_id='nested_cross_validation_linear',
+    log_time=elapsed_time
+)
+
+# Grid values for C and gamma
+# gridHyperPar = {"C": [0.001, 0.01, 0.1, 1, 10, 100], "gamma": [0.00001, .0001, .001, .01, .1]}
+# accuracy, std_dev = nested_cross_validation(
+#     x_feat = X_features,
+#     y_target = y_target,
+#     grid_hyper_param = gridHyperPar,
+#     n_splits=10,
+#     kernel='rbf',
+#     func_id='nested_cross_validation_rbf',
+#     log_time=elapsed_time
+# )
+print(elapsed_time)
